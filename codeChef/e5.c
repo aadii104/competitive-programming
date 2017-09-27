0fc6fd725e05fbe6496315b7252cf57a2cd9b0f6
aadii104 <sharmaadi104@gmail.com>
2017-09-27 06:43:26 +0530

    26/9/17

---
+++ codeChef/e5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int main(){
+    char a[1000002];
+    int i, test, flag, mid, flagp;
+    scanf("%d\n", &test);
+    while(test--> 0){
+        flag = 1;
+        gets(a);
+        len = strlen(a);
+        flagp = 0;
+        for(i=0;i<len;i++){
+            if(a[i      ])
+        }
+    }
+}
